bc41ef94b04e1f6f686aab9334930b0dfe120be7
root <root@localhost.localdomain>
2024-04-12 11:05:07 +0700

    remove .env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('fullstack-express-react', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 })
 
